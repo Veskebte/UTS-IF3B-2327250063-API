b00b6e2b00e3efd080c07d0f6f759dd84f17872e
Veskebte <nathasyapernidyaashari_2327250063@mhs.mdp.ac.id>
2024-10-31 13:59:13 +0700

    Final submission (2)

--- menu-restoran/app/Http/Controllers/MenuController.php
+++ menu-restoran/app/Http/Controllers/MenuController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Menu;
-use Illuminare\Http\Response;
+use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
